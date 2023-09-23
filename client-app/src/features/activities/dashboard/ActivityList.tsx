d4bf504ce6ada4ab33b0a5abe34b7546facc29c4
Peter Dennis <peeweecookiehead@gmail.com>
2023-09-22 21:42:33 -0400

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,13 +1,22 @@
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import { SyntheticEvent, useState } from "react";
 
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList ({activities, selectActivity, deleteActivity}: Props) {
+export default function ActivityList ({activities, selectActivity, deleteActivity, submitting}: Props) {
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
+
     return (
         <Segment>
             <Item.Group divided>
@@ -22,7 +31,13 @@ export default function ActivityList ({activities, selectActivity, deleteActivit
                             </Item.Description>
                             <Item.Extra>
                                 <Button floated='right' content='View' color='blue' onClick={() => selectActivity(activity.id)} />
-                                <Button floated='right' content='Delete' color='red' onClick={() => deleteActivity(activity.id)} />
+                                <Button
+                                    name={activity.id}
+                                    loading={submitting && target == activity.id}
+                                    floated='right'
+                                    content='Delete'
+                                    color='red'
+                                    onClick={(e) => handleActivityDelete(e, activity.id)} />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
